db19158a73a961e21ae7dd919cccc085629c391b
jlyheden <johan@lyheden.com>
2013-03-13 20:15:36 +0100

    added ubuntu precise support

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,7 @@ class r10k::params {
     'squeeze': {
       $r10k_bin = '/var/lib/gems/1.8/bin/r10k'
     }
-    'wheezy': {
+    'precise','wheezy': {
       $r10k_bin = '/usr/local/bin/r10k'
     }
   }
